79247b9e972f554ed53fcf78d3d2966a1435b6fc
Suraj Arya <s.arya844811@gmail.com>
2025-11-02 04:29:19 +0530

    .env and app.py updated

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from routes.chatbot_routes import chatbot_bp
+
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +9,9 @@ CORS(app)
 @app.route('/')
 def home():
     return jsonify({"message": "CleanCity Portal Backend Running 🚀"})
+
+app.register_blueprint(chatbot_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
